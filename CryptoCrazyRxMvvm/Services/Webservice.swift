a4b13cd334bcf2d758b7f6e471f7778341d7db2e
FurkanMDemiray <furkanmdemiray@gmail.com>
2023-12-13 21:24:43 +0300

    added ViewModel & RxSwift

--- CryptoCrazyRxMvvm/Services/Webservice.swift
+++ CryptoCrazyRxMvvm/Services/Webservice.swift
@@ -19,8 +19,18 @@ class Webservice {
     func downloadCurrcies(url: URL, completion: @escaping (Result<[Crypto], CryptoError>) -> ()) {
 
         URLSession.shared.dataTask(with: url) { data, response, error in
+            if let _ = error {
+                completion(.failure(.serverError))
+            } else if let data = data {
+                let cryptoList = try? JSONDecoder().decode([Crypto].self, from: data)
+                if let cryptoList = cryptoList {
+                    completion(.success(cryptoList))
+                } else {
+                    completion(.failure(.parsingError))
+                }
+            }
 
-        }
+        }.resume()
 
     }
 
